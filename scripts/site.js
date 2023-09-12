0fd7b78ffbcd1abd5d4674d9bf4ef93c1c14dba9
SANJAY M <sanjaym1304@gmail.com>
2023-09-12 19:37:28 +0530

    bootstrap

--- scripts/site.js
+++ scripts/site.js
@@ -1,16 +1,27 @@
-function Clearform(){
+function Clearform() {
   //  document.getElementById("name").nodeValue=' ';
-    document.getElementById("contactform").onreset();
-  }
+  document.getElementById("contactform").onreset();
+}
 
-  function hidePtags(){
-    let ptags = document.getElementsByTagName('p');
-    for(let item of ptags)
-    {
-     // item.style.visibility = 'hidden';
-      item.style.display= 'none';
-    }
+function hidePtags() {
+  let ptags = document.getElementsByTagName("p");
+  for (let item of ptags) {
+    // item.style.visibility = 'hidden';
+    item.style.display = "none";
   }
-  $('#jqueryBtn').click(function(){
-    $('p').hide();
+}
+$("#jqueryBtn").click(function () {
+  $("p").hide();
+});
+
+$(function(){
+  var pages = ['index','about','contact'];
+  var pathname = window.location.pathname;
+
+  $('.nav-link').each(function(item){
+      if(pathname.includes(pages[item])){
+          $(this).addClass('active');
+          $(this).attr('aria-current', 'page');
+      }
   })
+});
